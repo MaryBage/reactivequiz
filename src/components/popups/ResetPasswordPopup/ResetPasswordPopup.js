534beedc57d1ae232fd18294013ce178b324ece1
Arusik <grigorianarusik@gmail.com>
2020-08-03 15:42:04 +0400

    BackButton adding in ResetPassword component

--- src/components/popups/ResetPasswordPopup/ResetPasswordPopup.js
+++ src/components/popups/ResetPasswordPopup/ResetPasswordPopup.js
@@ -3,11 +3,14 @@ import { approveButtonText } from "../../../StaticContent";
 import s from "./PopupReset.module.css";
 import InformativeField from '../../pages/DetailedComponents/Fields/InformativeField/InformativeField';
 import CustomButton from '../../pages/DetailedComponents/Buttons/CustomButton/CustomButton';
+import { Link } from 'react-router-dom';
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
 
 const ResetPasswordPopup = (props) => {
     return (
         <div className="popup-container">
             <div className={s.resetWrapper}>
+            <CustomButton small="true" component={Link} to="/"><KeyboardBackspaceIcon/>back</CustomButton>
                 <div className={s.popupWrapper}>
                     <h1>Forgot your password?</h1>
                     <h3>
