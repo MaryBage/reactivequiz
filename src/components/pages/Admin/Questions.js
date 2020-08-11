0881aa3bc090a7c1a4ee04c9cfa19acda4f80750
Arusik <grigorianarusik@gmail.com>
2020-08-11 16:57:00 +0400

    Questions component / QuestionItems / extend and  apply buttons  / crsp css

--- src/components/pages/Admin/Questions.js
+++ src/components/pages/Admin/Questions.js
@@ -1,45 +1,49 @@
-import React, {useContext,useEffect} from 'react'
-import './Admin.css'
+import React, { useContext, useEffect } from 'react';
+import './Admin.css';
+import s from "./Questions.module.css";
 import { UserContext } from './context/user/userContext';
 import { DbContext } from './context/database/dbContext';
 import { Loader } from '../DetailedComponents/Loader/Loader';
-import {TransitionGroup, CSSTransition} from 'react-transition-group'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import DoneIcon from '@material-ui/icons/Done';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 
 const Questions = () => {
-    const {id} = useContext(UserContext)
-    const {getData, deleteData, loading, questions} = useContext(DbContext);
+    const { id } = useContext(UserContext);
+    const { getData, deleteData, loading, /*questions*/ } = useContext(DbContext);
+
+    const questions = ["what is it 1", "what is it 2", "what is it 3"]
 
     useEffect(() => {
         getData();
-        console.log('sadasd')
-      // eslint-disable-next-line
+        // eslint-disable-next-line
     }, [])
 
-return (
+    return (
         <>
-         { loading
-        ? <Loader />
-        :    <TransitionGroup component='ul' className='questions'>
-            
-                    {questions.map(questionItem => (
-                        <CSSTransition 
+            {loading ? <Loader />
+            : <TransitionGroup component='ul' className={s.questions}>
+                {questions.map(questionItem => (
+                    <CSSTransition
                         key={questionItem.questionId}
-                        classNames={'questionItem'}
+                        className={s.questionItem}
                         timeout={800}>
-                            <li className='questionItem'> 
-                                
-                                <div> <input type='checkbox' value={questionItem.questionId} /> <strong>{questionItem.question}</strong></div>
-                                <div  className="pointer red" onClick={() => deleteData(questionItem.questionDbId)}>&#10008;</div>
-                               
-                            </li>
-                        </CSSTransition>
-                    )) }
-                    
-             </TransitionGroup>
-      }
+                        <li className={s.questionItem}>
+                            <div className={s.inputWrapper}> 
+                                <input type='checkbox' value={questionItem.questionId} /> 
+                                <input type="text" value={questionItem} className={s.questionItemInput}/>
+                            </div>
+                            <div className={s.tick}><ArrowDropDownIcon style={{color:"#555555"}} /></div>
+                            <div className={s.tick}><DoneIcon style={{color:"#009900"}} /></div>
+                            <div className="pointer red" onClick={() => deleteData(questionItem.questionDbId)}>&#10008;</div>
+                        </li>
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
+            }
         </>
-
     )
-
 }
+
 export default Questions;
